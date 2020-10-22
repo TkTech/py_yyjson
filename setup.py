4305b86098bcf4354b1910eede048c6589d59ffd
Tyler Kennedy <tk@tkte.ch>
2020-10-22 00:42:03 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(root, 'README.md'), 'rb') as readme:
 setup(
     name='yyjson',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='yyjson bindings for python',
     long_description=long_description,
     long_description_content_type='text/markdown',
