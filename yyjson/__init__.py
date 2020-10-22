af8e3e8a02c07eb9a43d032dbb79500087d9109c
Tyler Kennedy <tk@tkte.ch>
2020-10-22 17:06:58 -0400

    Add stubs for load and loads, just to get benchmarks going.

--- yyjson/__init__.py
+++ yyjson/__init__.py
@@ -3,3 +3,13 @@ __all__ = [
 ]
 
 from cyyjson import Document
+
+
+def load(fp, *, cls=None, object_hook=None, parse_float=None, parse_int=None,
+         parse_constant=None, object_pairs_hook=None, **kwargs):
+    return Document(content=fp.read()).as_obj
+
+
+def loads(s, *, cls=None, object_hook=None, parse_float=None, parse_int=None,
+          parse_constant=None, object_pairs_hook=None, **kwargs):
+    return Document(content=s).as_obj
